51bc6e0bc7fee49145adcd41b89bda6ab9258220
Aliaksandr Khudnitski <alekskhudnitskiy@gmail.com>
2021-03-27 03:41:45 +0300

    feat: add solutions for extended-repeater and recursive-depth tasks

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,29 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater(str, options ) {
+  const baseString = str,
+    repeatTimes = options.repeatTimes,
+    separator = options.separator || '+',
+    addition = options.addition,
+    additionRepeatTimes = options.additionRepeatTimes,
+    additionSeparator = options.additionSeparator || '|';
+  let finalAdditionalString = '';
+  let finalBaseString = ''
+  let finalString = '';
+
+  if (addition !== undefined) {
+    finalAdditionalString = `${addition}`;
+    if (additionRepeatTimes !== undefined && typeof additionRepeatTimes === 'number' && additionRepeatTimes >= 2) {
+      finalAdditionalString = finalAdditionalString.concat(additionSeparator);
+      finalAdditionalString = finalAdditionalString.repeat(additionRepeatTimes - 1).concat(addition);
+    }
+  }
+
+  finalBaseString =`${baseString}${finalAdditionalString}`;
+  if (repeatTimes !== undefined && typeof repeatTimes === 'number' && repeatTimes >= 2) {
+    finalBaseString = finalBaseString.concat(separator);
+    return finalString.concat(finalBaseString.repeat(repeatTimes - 1), baseString, finalAdditionalString);
+  } else {
+    return finalBaseString;
+  }
 };
-  
